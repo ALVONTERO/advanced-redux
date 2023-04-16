925f2ed8c37e3b291a50a623ee8244cbd5343e97
ALVONTERO <83576414+ALVONTERO@users.noreply.github.com>
2023-04-16 13:49:07 +0200

    catch errors

--- src/App.js
+++ src/App.js
@@ -1,30 +1,76 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useEffect } from "react";
 import axios from "axios";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const fetch = async () => {
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      //put request
       const response = await axios.put(
         "https://react-http-base-default-rtdb.firebaseio.com/cart.json",
         { cart }
       );
-      return response
-    };
-    fetch();
-  }, [cart]);
+      if (!response.status === 200) {
+        throw new Error("Sending cart data failed.");
+      }
+      console.log(response)
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    }; //End of put request
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
